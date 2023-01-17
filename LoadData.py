a93dac3d2aa9ced41df302c3a9fb9da6761dfc67
GUI_ <51920543+GUI-nephelo@users.noreply.github.com>
2023-01-17 13:04:13 +0000

    fix

--- LoadData.py
+++ LoadData.py
@@ -10,12 +10,12 @@ def Con2Numpy(var_name):
     x1 = x.astype(float)
     return x1
 
-def load_HSI_data( data_name ):
+def load_HSI_data( data_name ,num):
     Data = dict()
-    img_gyh = data_name+'_gyh'
+    img_gyh = data_name+''
     img_gt = data_name+'_gt'
     Data['useful_sp_lab'] = np.array(Con2Numpy('useful_sp_lab'), dtype='int')
     Data[img_gt] = np.array(Con2Numpy(img_gt), dtype='int')
     Data[img_gyh] = Con2Numpy(img_gyh)
-    Data['trpos'] = np.array(Con2Numpy('trpos'), dtype='int')
+    Data[f'trpos{num}'] = np.array(Con2Numpy(f'trpos{num}'), dtype='int')
     return Data
